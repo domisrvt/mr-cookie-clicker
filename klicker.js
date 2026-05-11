f2fe5f02779684c80b72d97d47b2f13017ecb10d
Domeniks Seipuls <domeniksseipuls@gmail.com>
2026-05-11 11:25:25 +0300

    Charlie Klicker Update v2

--- klicker.js
+++ klicker.js
@@ -3,6 +3,10 @@ var score = 0;
 var clickValue = 1;
 var autoClickerEnabled = false;
 var autoClickerInterval;
+var autoClickerCost = 50;
+var autoClickerSpeed = 0;
+var autoClickerLevel = 0;
+var autoClickerUpgradeCost = 30;
 var upgradeCount = 0;
 var upgradeCost = 10;
 var upgradeIncrease = 1;
@@ -55,13 +59,16 @@ function updateUpgradeButton() {
     }
 }
 
-function buyAutoClicker(cost, initialSpeed) {
-    if (score >= cost && !autoClickerEnabled) {
-        score -= cost;
+function buyAutoClicker() {
+    if (score >= autoClickerCost && !autoClickerEnabled) {
+        score -= autoClickerCost;
         autoClickerEnabled = true;
+        autoClickerLevel = 1;
+        autoClickerSpeed = 1;
         document.getElementById('autoClickerStatus').innerText = 'Auto-Clicker: On';
-        autoClickerInterval = setInterval(autoClick, 1000 / initialSpeed);
+        autoClickerInterval = setInterval(autoClick, 1000 / autoClickerSpeed);
         updateScore();
+        updateAutoClickerButtons();
         checkAchievements();
     } else if (autoClickerEnabled) {
         alert('Auto-Clicker is already enabled!');
@@ -70,15 +77,35 @@ function buyAutoClicker(cost, initialSpeed) {
     }
 }
 
-function buyUpgradeAutoClicker(cost, speedIncrease) {
-    if (score >= cost && autoClickerEnabled) {
-        score -= cost;
+function buyUpgradeAutoClicker() {
+    if (!autoClickerEnabled) {
+        alert('Buy the Auto-Clicker first!');
+        return;
+    }
+
+    if (score >= autoClickerUpgradeCost) {
+        score -= autoClickerUpgradeCost;
+        autoClickerLevel++;
+        autoClickerSpeed = Math.max(1, autoClickerSpeed * 2);
+        autoClickerUpgradeCost = Math.ceil(autoClickerUpgradeCost * 2.5);
         clearInterval(autoClickerInterval);
-        autoClickerInterval = setInterval(autoClick, 1000 / (1 + speedIncrease));
+        autoClickerInterval = setInterval(autoClick, 1000 / autoClickerSpeed);
         updateScore();
+        updateAutoClickerButtons();
         checkAchievements();
     } else {
-        alert('Either Auto-Clicker is not enabled, or not enough cookies to buy this upgrade!');
+        alert('Not enough cookies to upgrade the Auto-Clicker!');
+    }
+}
+
+function updateAutoClickerButtons() {
+    var buyBtn = document.getElementById('buyAutoClickerBtn');
+    var upgradeBtn = document.getElementById('upgradeAutoClickerBtn');
+    if (buyBtn) {
+        buyBtn.innerText = 'Buy Auto-Clicker (Cost: ' + autoClickerCost + ' cookies)';
+    }
+    if (upgradeBtn) {
+        upgradeBtn.innerText = 'Upgrade Auto-Clicker (Cost: ' + autoClickerUpgradeCost + ' cookies)';
     }
 }
 
@@ -88,4 +115,5 @@ function autoClick() {
     checkAchievements();
 }
 
+updateAutoClickerButtons();
 updateUpgradeButton();
